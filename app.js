431c1a06d463364e7e9f6d6a9817767b63e804da
Shashank Naik <shashanknaik808@gmail.com>
2023-12-15 11:42:30 +0530

    Created my 1st requesthandler for index.hbs

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
 //Temlating Engine
-app.engine('hbs', exphbs({ extname: '.hbs' }));
+app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.listen(5000, () => {
     console.log("Server Started at Port 5000");
